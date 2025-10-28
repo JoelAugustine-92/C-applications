cf8cc9e93400848e67cb620a6659b93e325d0770
Joel Augustine <joelaugustine526@gmail.com>
2025-10-28 19:54:04 +0530

    Improve deque operations and structure
    Refactor deque functions for better readability and functionality.

--- Deque.c/Deque.c
+++ Deque.c/Deque.c
@@ -1,132 +1,153 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define size 5
 
-int deque[size],front=-1,rear=-1;
+int deque[size];
+int front = -1, rear = -1;
 
-void insertfont(int val)
-{
-  if((rear+1)%size == front)
-    printf("The queue is full");
-  else if(front == -1)
-    front = rear = -1;
-  else
-    rear=(rear+1)%size;
-  deque[rear]=val;
-}
-
-void insertend(int val)
-{
-  if((rear+1)%size == front)
-    printf("The queue is full");
-  else if(front == -1)
-    front = rear = -1;
-  else if(front == 0)
-    front = size -1;
-  else
-    front--;
-  deque[front]=val;
+// Insert at rear end
+void insertRear(int val) {
+    if ((rear + 1) % size == front) {
+        printf("Deque is full\n");
+        return;
+    }
+    if (front == -1) {
+        front = rear = 0;
+    } else {
+        rear = (rear + 1) % size;
+    }
+    deque[rear] = val;
+    printf("%d inserted at rear\n", val);
 }
 
-void deletefront(){
-  if(front == -1 && rear == -1)
-    printf("The queue is empty");
-  else if(front == rear)
-    front=(front+1)%size; 
+// Insert at front end
+void insertFront(int val) {
+    if ((rear + 1) % size == front) {
+        printf("Deque is full\n");
+        return;
+    }
+    if (front == -1) {
+        front = rear = 0;
+    } else if (front == 0) {
+        front = size - 1;  // wrap to end
+    } else {
+        front--;
+    }
+    deque[front] = val;
+    printf("%d inserted at front\n", val);
 }
-void deleteend(){
-  if(front == -1 && rear == -1)
-    printf("The queue is empty");
-  else if(front== rear)
-    front = rear = -1;
-  else if(rear == 0)
-    rear= size -1;
-  else
-    rear--;
-}
-void isfull(){
-  if((rear+1)%size == -1)
-    printf("The queue is full");
-  else
-    printf("The queue is not empty");
-}
-void isempty(){
-  if(front == -1 && rear == -1)
-    printf("The queue is empty");
-  else
-  {
-    printf("The queue has space");
-  }
-}
-void display()
-{
-  if(front == -1 && rear == -1)
-    printf("The queue is empty");
-  else
-    {
-      printf("The queue is: \n");
-      int i;
-      i=front;
-      while(1){
-        printf("%d",deque[i]);
-        if(i == rear)
-          break;
-        i=(i+1)%size;
-      }
+
+// Delete from front end
+void deleteFront() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+    printf("Deleted from front: %d\n", deque[front]);
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % size;
     }
 }
-int main(){
-  int val,choice;
-  while(1){
-    printf("***********doubly linked list ************************\n");
-    printf("1)Insert at front\n");
-    printf("2)Insert at end\n");
-    printf("3)Delete from front\n");
-    printf("4)Delete from end\n");
-    printf("5)Is full\n");
-    printf("6)Is empty\n");
-    printf("7)display\n");
-    printf("8)exit");
-    printf("\n Enter ur choice");
-    scanf("%d",&choice);
-    switch(choice){
-      case 1:
-            printf("Enter the element: \t");
-            scanf("%d",&val);
-            insertfont(val);
+
+// Delete from rear end
+void deleteRear() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+    printf("Deleted from rear: %d\n", deque[rear]);
+    if (front == rear) {
+        front = rear = -1;
+    } else if (rear == 0) {
+        rear = size - 1;
+    } else {
+        rear--;
+    }
+}
+
+// Check full
+void isFull() {
+    if ((rear + 1) % size == front)
+        printf("Deque is full\n");
+    else
+        printf("Deque is not full\n");
+}
+
+// Check empty
+void isEmpty() {
+    if (front == -1)
+        printf("Deque is empty\n");
+    else
+        printf("Deque is not empty\n");
+}
+
+// Display contents
+void display() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+    printf("Deque elements: ");
+    int i = front;
+    while (1) {
+        printf("%d ", deque[i]);
+        if (i == rear)
             break;
-      case 2:
-            printf("Enter the element: \t");
-            scanf("%d",&val);
-            insertend(val);
-             break;
-      case 3:
-            deletefront();
-             break;
-      case 4:
-            deleteend();
-             break;
-      case 5:
-            isfull();
-             break;
-      case 6:
-            isempty();
-             break;
-      case 7:
-            display();
-             break;
-      case 8:
-            printf("Exiting...");
-            exit(0);
-            return 0;
-             break;
-      default:
-            printf("Invalid choice!");
-             break;
+        i = (i + 1) % size;
     }
-  }
-  return 0;
+    printf("\n");
 }
 
+int main() {
+    int val, choice;
+    while (1) {
+        printf("\n************ CIRCULAR DEQUE **************\n");
+        printf("1) Insert Front\n");
+        printf("2) Insert Rear\n");
+        printf("3) Delete Front\n");
+        printf("4) Delete Rear\n");
+        printf("5) Is_Full\n");
+        printf("6) Is_Empty\n");
+        printf("7) Display\n");
+        printf("8) Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert at front: ");
+                scanf("%d", &val);
+                insertFront(val);
+                break;
+            case 2:
+                printf("Enter value to insert at rear: ");
+                scanf("%d", &val);
+                insertRear(val);
+                break;
+            case 3:
+                deleteFront();
+                break;
+            case 4:
+                deleteRear();
+                break;
+            case 5:
+                isFull();
+                break;
+            case 6:
+                isEmpty();
+                break;
+            case 7:
+                display();
+                break;
+            case 8:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
